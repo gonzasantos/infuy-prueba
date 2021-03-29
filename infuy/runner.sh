a02fd7c01a43bbd9fc3b432cb36c737448ac21e3
Gonzalo Santos <gonzasantos12@gmail.com>
2021-03-29 19:30:08 -0300

    Actualizo runner.sh para usar uwsgi

--- infuy/runner.sh
+++ infuy/runner.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
-#uwsgi --http "0.0.0.0:8000" --module infuy.wsgi
-python manage.py runserver 0:8000
+set -e
+while ! nc -z db 5432; do sleep 5; done;
+python manage.py migrate
+
+uwsgi --http "0.0.0.0:8000" --module infuy.wsgi
+#python manage.py runserver 0:8000
